d84631168ac036622853123eacf24dcd146cb4b1
Andrew Fontana <32223252+Kharg@users.noreply.github.com>
2025-08-25 23:56:21 +0200

    1.0.0

---
+++ files/client/custom/modules/progress-tracker/src/views/fields/progress-tracker.js
@@ -0,0 +1,51 @@
+define('progress-tracker:views/fields/progress-tracker', ['views/fields/enum'], (Dep) => {
+    return class extends Dep {
+        type = 'progressTracker'
+        detailTemplate = 'progress-tracker:views/fields/progress-tracker'
+        editTemplate = 'fields/enum/edit'
+        searchTemplate = 'fields/enum/search'
+
+        setup() {
+            super.setup();
+            this.listenTo(this.model, 'change:' + this.name, () => {
+                this.reRender();
+            });
+        }
+
+        data() {
+            const fieldValue = this.model.get(this.name);
+            const values = this.options.options || this.params.options || [];
+            const mode = this.params.mode || 'progress-tracker';
+            const currentIndex = values.indexOf(fieldValue);
+            const color = this.params.color || '#0f62fe';
+
+            const modifiedValues = values.map((value, index) => {
+                const label = this.getLanguage().translateOption(
+                    value,
+                    this.name,
+                    this.model.entityType
+                );
+
+                const isComplete = index < currentIndex;
+                const isActive = index === currentIndex;
+
+                return {
+                    ...super.data(),
+                    value: value,
+                    label: label,
+                    isComplete: isComplete,
+                    isActive: isActive,
+                };
+            });
+
+            return _.extend({
+                fieldValue: fieldValue,
+                mode: mode,
+                color: color,
+                values: modifiedValues,
+                originalValues: values,
+                currentIndex: currentIndex
+            }, super.data());
+        }
+    };
+});
